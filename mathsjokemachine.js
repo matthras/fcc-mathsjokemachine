18120c60e036c3da809487bf3016b4dc5c7cae70
matthras <matthew.y.p.mack@gmail.com>
2017-04-14 10:37:14 +1000

    refactored initial load to save joke list to memory

--- mathsjokemachine.js
+++ mathsjokemachine.js
@@ -1,70 +1,53 @@
 // Client ID and API key from the Developer Console
 var CLIENT_ID = '751459220695-q93iap2b4hbn0j08jv3pb7h1lms4mkel.apps.googleusercontent.com';
-
 // Array of API discovery doc URLs for APIs used by the quickstart
 var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
-
 // Authorization scopes required by the API; multiple scopes can be
 // included, separated by spaces.
 var SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly";
-
 var authorizeButton = document.getElementById('authorize-button');
 var signoutButton = document.getElementById('signout-button');
-
-/**
- *  On load, called to load the auth2 library and API client library.
- */
+/* On load, called to load the auth2 library and API client library. */
 function handleClientLoad() {
-gapi.load('client:auth2', initClient);
+    gapi.load('client:auth2', initClient);
 }
-
-/**
- *  Initializes the API client library and sets up sign-in state
- *  listeners.
- */
+/* Initializes the API client library and sets up sign-in state listeners. */
 function initClient() {
-gapi.client.init({
-    discoveryDocs: DISCOVERY_DOCS,
-    clientId: CLIENT_ID,
-    scope: SCOPES
-}).then(function () {
-    // Listen for sign-in state changes.
-    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+    gapi.client.init({
+        discoveryDocs: DISCOVERY_DOCS,
+        clientId: CLIENT_ID,
+        scope: SCOPES
+    }).then(function () {
+        // Listen for sign-in state changes.
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
 
-    // Handle the initial sign-in state.
-    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-    authorizeButton.onclick = handleAuthClick;
-    signoutButton.onclick = handleSignoutClick;
-});
+        // Handle the initial sign-in state.
+        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        authorizeButton.onclick = handleAuthClick;
+        signoutButton.onclick = handleSignoutClick;
+    });
 }
-
-/**
- *  Called when the signed in status changes, to update the UI
- *  appropriately. After a sign-in, the API is called.
- */
+/*  Called when the signed in status changes, to update the UI appropriately. 
+    After a sign-in, the API is called. */
 function updateSigninStatus(isSignedIn) {
 if (isSignedIn) {
     authorizeButton.style.display = 'none';
     signoutButton.style.display = 'block';
-    listMajors();
+    loadJokesIntoMemory();
+    //listMajors();
 } else {
     authorizeButton.style.display = 'block';
     signoutButton.style.display = 'none';
 }
 }
 
-/**
- *  Sign in the user upon button click.
- */
+/* Sign in the user upon button click. */
 function handleAuthClick(event) {
-gapi.auth2.getAuthInstance().signIn();
+    gapi.auth2.getAuthInstance().signIn();
 }
-
-/**
- *  Sign out the user upon button click.
- */
+/* Sign out the user upon button click. */
 function handleSignoutClick(event) {
-gapi.auth2.getAuthInstance().signOut();
+    gapi.auth2.getAuthInstance().signOut();
 }
 
 /**
@@ -84,22 +67,53 @@ pre.appendChild(textContent);
  * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
  */
 function listMajors() {
-gapi.client.sheets.spreadsheets.values.get({
-    spreadsheetId: '1rulgef2EIt_2wLpuPTuLvLgP-ZzEmebRIKOzmMqiKHg',
-    range: 'MathsJokeSheet!A2:B',
-}).then(function(response) {
-    var range = response.result;
-    console.log(range);
-    if (range.values.length > 0) {
-    appendPre('Question, Answer:');
-    for (i = 0; i < range.values.length; i++) {
-        var row = range.values[i];
-        appendPre(row[0] + ', ' + row[1]);
-    }
-    } else {
-    appendPre('No data found.');
-    }
-}, function(response) {
-    appendPre('Error: ' + response.result.error.message);
-});
+    gapi.client.sheets.spreadsheets.values.get({
+        spreadsheetId: '1rulgef2EIt_2wLpuPTuLvLgP-ZzEmebRIKOzmMqiKHg',
+        range: 'MathsJokeSheet!A2:B',
+    }).then(function(response) {
+        var range = response.result;
+        console.log(range);
+        if (range.values.length > 0) {
+        appendPre('Question, Answer:');
+        for (i = 0; i < range.values.length; i++) {
+            var row = range.values[i];
+            appendPre(row[0] + ', ' + row[1]);
+        }
+        } else {
+        appendPre('No data found.');
+        }
+    }, function(response) {
+        appendPre('Error: ' + response.result.error.message);
+    });
+}
+
+// Initial setup 
+
+// Download the Joke List from the Google Sheets Doc and saves it to memory.
+var mathsjokeList;
+var nJokes;
+function loadJokesIntoMemory() {
+    gapi.client.sheets.spreadsheets.values.get({
+        spreadsheetId: '1rulgef2EIt_2wLpuPTuLvLgP-ZzEmebRIKOzmMqiKHg',
+        range: 'MathsJokeSheet!A2:B',
+    }).then(function(res) {
+        console.log(res);
+        mathsjokeList = res.result.values;
+        nJokes = res.result.values.length;
+        console.log(nJokes);
+        if(nJokes > 0){
+            document.getElementById('nJokes').innerText=nJokes;
+        } else {
+            document.getElementById('nJokesParagraph').innerText="This Maths Joke Machine is currently busted!";
+        }
+    }, function(response) {
+        document.getElementById('nJokesParagraph').innerText="This Maths Joke Machine is currently busted!";
+        console.log('Error: ' + response.result.error.message);
+    });
+}
+
+var jokeNumber;
+const generateJoke = () => {
+    jokeNumber = Math.floor(Math.random()*nJokes);
+    document.getElementById('jokeQuestion').innerText=mathsjokeList[jokeNumber][1];
 }
